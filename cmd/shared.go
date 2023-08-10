0f87d3e0d5f4a302a79111c1a4f6d6193783ef35
Johan Lindell <johan@lindell.me>
2023-08-10 13:20:08 +0200

    feat: filter devices based on manifests or predefined list

--- cmd/shared.go
+++ cmd/shared.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/lindell/connect-iq-sdk-manager-cli/internal/manager"
 	"github.com/lindell/connect-iq-sdk-manager-cli/internal/storage"
+	"github.com/pkg/errors"
 )
 
 func NewManager() (manager.Manager, error) {
@@ -14,4 +17,19 @@ func NewManager() (manager.Manager, error) {
 	return manager.Manager{
 		Store: store,
 	}, nil
+}
+
+func checkExlusivity(options map[string]bool) error {
+	enabledOptions := []string{}
+	for name, val := range options {
+		if val {
+			enabledOptions = append(enabledOptions, name)
+		}
+	}
+
+	if len(enabledOptions) <= 1 {
+		return nil
+	}
+
+	return errors.Errorf("can't define more than one of %s", strings.Join(enabledOptions, ", "))
 }
