25149789d15b0903d079163b1c8515c3993dbe61
Johan Lindell <johan@lindell.me>
2023-08-25 12:32:26 +0200

    tests: make sure monkeyc is installed

--- tests/live_test.go
+++ tests/live_test.go
@@ -1,12 +1,17 @@
 package tests
 
 import (
+	"bytes"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/lindell/connect-iq-sdk-manager-cli/cmd"
 	"github.com/lindell/connect-iq-sdk-manager-cli/internal/connectiq"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestStory(t *testing.T) {
@@ -38,11 +43,32 @@ func TestStory(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Greater(t, len(files), 10)
 
-	assert.NoError(t, run("sdk", "download", "^6.0.0"))
+	assert.NoError(t, run("sdk", "set", "^6.0.0"))
+
+	_, _, err = execCmd("go", "install", "..")
+	require.NoError(t, err)
+
+	stdOut, _, err := execCmd("connect-iq-sdk-manager-cli", "sdk", "current-path", "--bin")
+	require.NoError(t, err)
+	binPath := strings.TrimSpace(stdOut)
+
+	monkeyCBin := filepath.Join(binPath, "./monkeyc")
+	_, _, err = execCmd(monkeyCBin, "--help")
+	require.NoError(t, err)
 }
 
 func run(args ...string) error {
 	command := cmd.RootCmd()
 	command.SetArgs(args)
 	return command.Execute()
+}
+
+func execCmd(cmd string, args ...string) (string, string, error) {
+	c := exec.Command(cmd, args...)
+	stdout := bytes.NewBuffer(nil)
+	stderr := bytes.NewBuffer(nil)
+	c.Stdout = stdout
+	c.Stderr = stderr
+	err := c.Run()
+	return stdout.String(), stderr.String(), err
 }
